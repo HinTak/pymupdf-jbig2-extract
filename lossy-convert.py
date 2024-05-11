c9dac33ff4e6079a4518024f30b774c3c7ff9a14
Hin-Tak Leung <htl10@users.sourceforge.net>
2024-05-11 22:37:42 +0100

    Lossy-convert, first working version
    Use fitz.Pixmap to convert instead of PIL.Image

---
+++ lossy-convert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Copyright 2024 Hin-Tak Leung
+
+# extract all image masks from a pdf (consisting of scans, many from LuraDocument)
+
+import sys
+import os
+
+import fitz
+
+from fitz import Pixmap
+
+import io
+
+fname = sys.argv[1] if len(sys.argv) == 2 else None
+if not fname:
+    raise SystemExit()
+
+doc = fitz.open(fname)
+
+page_count = doc.page_count
+
+for page in doc.pages():
+    pno = page.number
+    ImageList = page.get_images()
+    # 151 [(303, 0, 1280, 1920, 8, 'DeviceRGB', '', 'Im152', 'FlateDecode')]
+    if ((ImageList[0][8] == 'FlateDecode') and (ImageList[0][5] == 'DeviceRGB')):
+        xref = ImageList[0][0]
+        print(pno, xref)
+        pix = Pixmap(doc, xref)
+        tmpfilename = "tmp-%d-%d.jpg" % (pno, xref)
+        pix.save(tmpfilename)
+        page.replace_image(xref, filename=tmpfilename)
+        os.remove(tmpfilename)
+doc.save("1.pdf")
